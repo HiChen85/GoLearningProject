ef40a901c11f6c5d8f1c5321d2995b0e2cc70b3a
HiChen85 <hccc_zhang@outlook.com>
2020-10-06 12:58:09 +0800

    增加两个函数

--- Map/map1.go
+++ Map/map1.go
@@ -55,6 +55,8 @@ func main() {
 
 	// map不能用==来比较两个map是否相等,只能判断
 
+	sliceMap()
+	mapSlice()
 }
 
 // 此函数测试直接传入一个map的变量,对其修改后,原来的map是否会发生改变
@@ -64,4 +66,44 @@ func changeMap(m map[int]string) {
 		// fmt.Println(k, v)
 		m[k] = v + strconv.Itoa(k)
 	}
+}
+
+func sliceMap() {
+	sm := make([]map[string]string, 4)
+	fmt.Println(sm)
+
+	// 使用此段代码的结果:
+	// map[] map[] map[] map[] map[]]
+	// 原因是因为forrange循环中的第二个变量为一个拷贝副本,无法真正修改引用类型的值
+	// for idx, val := range sm {
+	// 	// val[fmt.Sprint(idx)] = fmt.Sprint(idx + 1) // 无法直接向未分配空间的map中添加元素.需要进行初始化
+	// 	val = make(map[string]string, idx+1)
+	// 	val[fmt.Sprint(idx)] = fmt.Sprint(idx + 1)
+	// }
+
+	mp1 := make(map[string]string, 3)
+	sm[0] = mp1
+	mp1["1"] = "你好"
+
+	// for i := 0; i < len(sm); i++ {
+	// 	sm[i] = make(map[string]string, i+1)
+	// 	sm[i][fmt.Sprint(i)] = fmt.Sprint(i + 1)
+	// }
+	fmt.Printf("%#v\n", sm)
+}
+
+// 元素为slice的map.
+// 不管是什么引用类型, 在声明后都要初始化,否则无法向引用变量中增加内容
+func mapSlice() {
+	sli1 := []int{1, 3, 5}
+	sli2 := []int{2, 4, 6}
+	sli3 := []int{3, 5, 7}
+	ms := make(map[string][]int, 3)
+	ms["1"] = sli1
+	ms["2"] = sli2
+	ms["3"] = sli3
+	fmt.Printf("%#v\n", ms)
+	ms["3"][2] = 999
+	fmt.Printf("%#v\n", ms)
+	fmt.Printf("%#v\n", sli3)
 }
